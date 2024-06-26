16a370725964697cfbe142877a3bba0a1d2ec600
FullStackOS <alex.youssef@live.com>
2024-06-26 10:04:53 +0200

    implemented book class

--- src/main/java/fullstackOS/org/Book.java
+++ src/main/java/fullstackOS/org/Book.java
@@ -1,4 +1,51 @@
 package fullstackOS.org;
 
 public class Book {
+        private String id;
+        private String title;
+        private String author;
+        private boolean available;
+        private Person borrower;
+
+        public Book(String id, String title, String author) {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.available = true;
+            this.borrower = null;
+        }
+
+        public String getBookInformation() {
+            return "Book ID: " + id + ", Title: " + title + ", Author: " + author + ", Available: " + available;
+        }
+
+        // the getter and setters
+        public String getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public boolean isAvailable() {
+            return available;
+        }
+
+        public void setAvailable(boolean available) {
+            this.available = available;
+        }
+
+        public Person getBorrower() {
+            return borrower;
+        }
+
+        public void setBorrower(Person borrower) {
+            this.borrower = borrower;
+        }
+    }
 }
